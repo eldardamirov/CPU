43c5421294130789f95635c4ef59f602a7672d97
eldardamirov <eldardamirov75@mail.ru>
2018-08-18 13:36:03 +0300

    Core file added.

---
+++ CPU/core.cpp
@@ -0,0 +1,9 @@
+//
+//  core.cpp
+//  CPU
+//
+//  Created by Эльдар Дамиров on 18.08.2018.
+//  Copyright © 2018 Эльдар Дамиров. All rights reserved.
+//
+
+#include "core.hpp"
---
+++ CPU/core.hpp
@@ -0,0 +1,14 @@
+//
+//  core.hpp
+//  CPU
+//
+//  Created by Эльдар Дамиров on 18.08.2018.
+//  Copyright © 2018 Эльдар Дамиров. All rights reserved.
+//
+
+#ifndef core_hpp
+#define core_hpp
+
+#include <stdio.h>
+
+#endif /* core_hpp */
